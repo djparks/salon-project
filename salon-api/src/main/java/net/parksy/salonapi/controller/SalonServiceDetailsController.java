525f469f3662480d7ee24532c61168b673c352e7
Darryl Parks <djparks@usa.net>
2022-03-19 17:48:29 -0500

    Refactor name

--- salon-api/src/main/java/net/parksy/salonapi/controller/SalonServiceDetailsController.java
+++ salon-api/src/main/java/net/parksy/salonapi/controller/SalonServiceDetailsController.java
@@ -2,13 +2,9 @@ package net.parksy.salonapi.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import net.parksy.salonapi.SalonDetails;
 import net.parksy.salonapi.data.SalonServiceDetailRepository;
 import net.parksy.salonapi.entity.SalonServiceDetail;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/services")
 @CrossOrigin("*")
 @Api(value="salonstore", description="Operations pertaining to Salon Details")
-public class SalonDetailsController {
+public class SalonServiceDetailsController {
     @Autowired
     private SalonServiceDetailRepository salonServiceDetailRepo;
 
